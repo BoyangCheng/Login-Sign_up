95c7ddf3c3da7dc1467466d9d3751f7c96e7f5e4
BoyangCheng <bcheng4@jhu.edu>
2017-12-31 21:39:52 -0500

    1

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -9,6 +9,17 @@ class Auth extends CI_Controller
     {
       $this->load->view('login');
     }
+	    public function sign_up()
+    {
+		 $this->load->library('form_validation');
+	if(isset($_POST[username]))
+		$this->form_validation->set_rules('username','username','required');
+
+	if ($this->form_validation->run()==true)
+		echo "sign up successfully";
+      $this->load->view('sign_up');
+  
+    }
 
 }
 
--- application/views/sign_up.php
+++ application/views/sign_up.php
@@ -8,12 +8,15 @@
     <img src="${pageContext.request.contextPath}/views/images/welcome.png">
     <h3>please fill your information to join.</h3>
 
-    <form>
+	<?php echo validation_errors('<div>','</div>'); ?>
+    <form action='' method='POST'>
 
         <table>
             <tr><td>Username：</td><td><input type="text" name="username"></td></tr>
 
             <tr><td>Password：</td><td><input type="password" name="password"></td></tr>
+			
+			<tr><td>Confirm password：</td><td><input type="password" name="confirm_password"></td></tr>
 
             <tr><td>E-mail：</td><td><input type="text" name="email"></td></tr>
         </table>
