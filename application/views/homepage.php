05c9b8022b59d151e0a8cd9b434727e512d0fa51
BoyangCheng <bcheng4@jhu.edu>
2018-01-06 23:16:18 -0500

    1
    1

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,12 +1,49 @@
 <html>
 <head>
   <title>Homepage</title>
+  <style>
+  .success
+	{
+		background-color:green; 
+		color:white;
+	}
+	.left{
+            float:left;
+            padding-left:10px;
+        }
+	.mid
+	{
+		
+		width:10%;
+		margin:0 auto;
+		font-size:30px
+		
+	}
+	 a
+	 {
+          text-decoration: none;
+     }
+     a:hover 
+	 { 
+		 text-decoration:underline;
+		 color: gray
+	 }
+
+  </style>
 </head>
 <body style="text-align:center">
+
 <?php if (isset($_SESSION['success'])){?>
-<div><p style="color:pink"> <?php echo $_SESSION['success']  ?></p></div>
-<div>
+<div class="success"><p> <?php echo $_SESSION['success']  ?></p></div>
+<img src="<?php echo base_url('images/success.png'); ?>" width="200px" />
+
+
+<div style="font-size:30px">
   welcome,<?php echo $_SESSION['username']  ?>
+  <br>
+  <div class="mid">
+	<a href="<?php echo site_url('Auth/logout') ?>" ><img src="<?php echo base_url('images/logout.png'); ?>" class="left" width="50px" />logout</a>
+  </div>
 </div>
 <?php }?>
 </body>
